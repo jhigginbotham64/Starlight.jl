242f8f49c6266195565d1b97635f049f4363e54d
Joshua Higginbotham <jhigginbotham64@gmail.com>
2020-12-16 14:31:49 -0600

    today i learned how to run unit tests in julia, and not to use julia pro's bundled interpreter

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,70 @@
 using starlight
 using Test
 
-@testset "starlight.jl" begin
-    # Write your tests here.
+@testset "ray tracer challenge" begin
+
+    @testset "ch 1 - tuples, points, and vectors" begin
+        @test starlight.greet() == nothing
+    end
+
+    @testset "ch 2 - drawing on a canvas" begin
+
+    end
+
+    @testset "ch 3 - matrices" begin
+
+    end
+
+    @testset "ch 4 - matrix transformations" begin
+
+    end
+
+    @testset "ch 5 - ray-sphere intersections" begin
+
+    end
+
+    @testset "ch 6 - light and shading" begin
+
+    end
+
+    @testset "ch 7 - making a scene" begin
+
+    end
+
+    @testset "ch 8 - shadows" begin
+
+    end
+
+    @testset "ch 9 - planes" begin
+
+    end
+
+    @testset "ch 10 - patterns" begin
+
+    end
+
+    @testset "ch 11 - reflection and refraction" begin
+
+    end
+
+    @testset "ch 12 - cubes" begin
+
+    end
+
+    @testset "ch 13 - cylinders" begin
+
+    end
+
+    @testset "ch 14 - groups" begin
+
+    end
+
+    @testset "ch 15 - triangles" begin
+
+    end
+
+    @testset "ch 16 - constructive solid geometry" begin
+
+    end
+
 end
