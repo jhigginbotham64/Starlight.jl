d898fe5e4bc3b12e15efb9a4429ad2d651f9caaa
Joshua Higginbotham <jhigginbotham64@gmail.com>
2020-12-17 11:18:10 -0600

    finished chapter 1, all tests passing

--- src/starlight.jl
+++ src/starlight.jl
@@ -1,6 +1,99 @@
 module starlight
 
-export greet
-greet() = println("hello, world!")
+#=
+    points and vectors are just length-4 arrays with particular values
+    in the last index
+=#
+
+export fitn
+export point
+export vector
+
+function fitn(vec::Vector, n::Int = 3)
+    """
+        fit vector to n elements, i.e. truncate or pad.
+        defaults to n = 3 because that's my use case.
+    """
+    return vcat(vec[1:min(n, length(vec))], repeat([0], max(0, n - length(vec))))
+end
+
+function point(coords)
+    return vcat(fitn(coords), [1])
+end
+
+function vector(coords)
+    return vcat(fitn(coords), [0])
+end
+
+#=
+    really all the important matrix operations can be done directly
+    with julia's built-in types, but we also like to be able to refer
+    to coordinate axes by name. fortunately julia makes it relatively
+    simple to add that on top of its builtins.
+=#
+
+function Base.getproperty(vec::Vector{<:Number}, sym::Symbol)
+    if sym === :x
+        if length(vec) >= 1
+            return vec[1]
+        else
+            @warn "property x is not defined for empty arrays"
+            return nothing
+        end
+    elseif sym === :y
+        if length(vec) >= 2
+            return vec[2]
+        else
+            @warn "property y is not defined for arrays with fewer than 2 elements"
+            return nothing
+        end
+    elseif sym === :z
+        if length(vec) >= 3
+            return vec[3]
+        else
+            @warn "property z is not defined for arrays with fewer than 3 elements"
+            return nothing
+        end
+    elseif sym === :w
+        if length(vec) >= 4
+            return vec[4]
+        else
+            @warn "property w is not defined for arrays with fewer than 4 elements"
+            return nothing
+        end
+    else
+        return getfield(vec, sym)
+    end
+end
+
+function Base.setproperty!(vec::Vector{<:Number}, sym::Symbol, val::T where T<:Number)
+    if sym === :x
+        if length(vec) >= 1
+            vec[1] = val
+        else
+            @warn "property x is not defined for empty arrays"
+        end
+    elseif sym === :y
+        if length(vec) >= 2
+            vec[2] = val
+        else
+            @warn "property y is not defined for arrays with fewer than 2 elements"
+        end
+    elseif sym === :z
+        if length(vec) >= 3
+            vec[3] = val
+        else
+            @warn "property z is not defined for arrays with fewer than 3 elements"
+        end
+    elseif sym === :w
+        if length(vec) >= 4
+            vec[4] = val
+        else
+            @warn "property w is not defined for arrays with fewer than 4 elements"
+        end
+    else
+        setfield!(vec, sym, val)
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,57 +7,91 @@ using LinearAlgebra
 @testset "ray tracer challenge" begin
 
     @testset "ch 1 - tuples, points, and vectors" begin
+
         #=
-            overload getproperty and setproperty! to implement x,y,z,w?
-            these new definitions would be exported, while the array wrapper
-            functions would not be to avoid conflicts with built-ins, especially
-            tuple.
+            ch 1 summary:
+
+            julia's native data types and standard library functions
+            turned this chapter into a smoke test of basic language features.
+            very different experience from python, but that may because i'm
+            older and have already done it once in python and am less inclined
+            to go off the rails.
+
+            it takes less than 100 lines of module code to make all these
+            tests pass. define getproperty and setproperty! for arrays of
+            numbers so you can use .x, .y, etc. then define point and vector
+            "constructors" which are basically just functions that take an
+            array and return the first 3 elements (or the list 0-padded to
+            length 3) and tack 1 or 0 respectively onto the end. tuples as
+            defined by the book become superfluous, even a hindrance, and this
+            is why we now have test cases which are basically just "julia can do
+            arithmetic on arrays, yaaaaaaay".
+
+            the one weird spot is that the standard library cross product is
+            only defined for arrays of length 3. the book's cross product is...
+            the same, but is only defined for vectors and ignores the last
+            element. i opted to cheat a little and just use the standard library
+            cross product without "creating a vector" first (see the last two
+            tests for what i mean). this is simple enough but requires me or
+            whoever else to remember something extra about how this code works.
+            i may come up with something else depending on how much i have to
+            use and abuse the cross product in this book.
+
+            whatever i do, my goal for now is to avoid breaking and/or
+            revisiting previous tests as much as possible, so there's a good
+            chance that it (and everything else from this chapter) will stay
+            the way it is.
+
+            i can't fault the book though. it's written to be language-agnostic,
+            and the same content has still proved very valuable in helping me
+            learn some of the julia fundamentals. remember, this is my first
+            time using this language. overall i'm fairly pleased with it so far.
         =#
 
         vals = [4.3, -4.2, 3.1, 1.0]
 
-        a = sl.tuple(vals...)
+        a = copy(vals)
         @test a.x == 4.3
         @test a.y == -4.2
         @test a.z == 3.1
         @test a.w == 1.0
-        @test a == sl.point(vals...)
-        @test a != sl.vector(vals...)
+        @test a == point(vals)
+        @test a != vector(vals)
 
         a.w = 0.0
         @test a.x == 4.3
         @test a.y == -4.2
         @test a.z == 3.1
         @test a.w == 0.0
-        @test a != sl.point(vals...)
-        @test a == sl.vector(vals...)
+        @test a != point(vals)
+        @test a == vector(vals)
 
-        @test sl.point(4, -4, 3) == sl.tuple(4, -4, 3, 1)
-        @test sl.vector(4, -4, 3) == sl.tuple(4, -4, 3, 0)
-        @test sl.tuple(3, -2, 5, 1) - sl.tuple(-2, 3, 1, 0) == sl.tuple(1, 1, 6, 1)
-        @test sl.point(3, 2, 1) - sl.point(5, 6, 7) == sl.vector(-2, -4, -6)
-        @test sl.point(3, 2, 1) - sl.vector(5, 6, 7) == sl.point(-2, -4, -6)
-        @test sl.vector(3, 2, 1) - sl.vector(5, 6, 7) == sl.vector(-2, -4, -6)
-        @test sl.vector(0, 0, 0) - sl.vector(1, -2, 3) == sl.vector(-1, 2, -3)
-        @test -sl.tuple(1, -2, 3, -4) == sl.tuple(-1, 2, -3, 4)
-        @test 3.5 * sl.tuple(1, -2, 3, -4) == sl.tuple(3.5, -7, 10.5, -14)
-        @test 0.5 * sl.tuple(1, -2, 3, -4) == sl.tuple(0.5, -1, 1.5, -2)
-        @test sl.tuple(1, -2, 3, -4) / 2 == sl.tuple(0.5, -1, 1.5, -2)
+        @test point([4, -4, 3]) == [4, -4, 3, 1]
+        @test vector([4, -4, 3]) == [4, -4, 3, 0]
+        @test [3, -2, 5, 1] + [-2, 3, 1, 0] == [1, 1, 6, 1]
+        @test point([3, 2, 1]) - point([5, 6, 7]) == vector([-2, -4, -6])
+        @test point([3, 2, 1]) - vector([5, 6, 7]) == point([-2, -4, -6])
+        @test vector([3, 2, 1]) - vector([5, 6, 7]) == vector([-2, -4, -6])
+        @test vector([0, 0, 0]) - vector([1, -2, 3]) == vector([-1, 2, -3])
+        @test -[1, -2, 3, -4] == [-1, 2, -3, 4]
+        @test 3.5 * [1, -2, 3, -4] == [3.5, -7, 10.5, -14]
+        @test 0.5 * [1, -2, 3, -4] == [0.5, -1, 1.5, -2]
+        @test [1, -2, 3, -4] / 2 == [0.5, -1, 1.5, -2]
 
-        @test norm(sl.vector(1, 0, 0)) == 1
-        @test norm(sl.vector(0, 1, 0)) == 1
-        @test norm(sl.vector(0, 0, 1)) == 1
-        @test norm(sl.vector(1, 2, 3)) == √14 # i love julia
-        @test norm(sl.vector(-1, -2, -3)) = √14
+        @test norm(vector([1, 0, 0])) == 1
+        @test norm(vector([0, 1, 0])) == 1
+        @test norm(vector([0, 0, 1])) == 1
+        @test norm(vector([1, 2, 3])) == √14 # i love julia
+        @test norm(vector([-1, -2, -3])) == √14
 
-        @test normalize(sl.vector(4, 0, 0)) == sl.vector(1, 0, 0)
-        @test normalize(sl.vector(1, 2, 3)) ≈ sl.vector(1/√14, 2/√14, 3/√14) # i really love julia
-        @test norm(normalize(sl.vector(1, 2, 3))) == 1
+        @test normalize(vector([4, 0, 0])) == vector([1, 0, 0])
+        @test normalize(vector([1, 2, 3])) == vector([1/√14, 2/√14, 3/√14]) # i really love julia
+        @test norm(normalize(vector([1, 2, 3]))) == 1
 
         # i really really really love julia
-        @test sl.vector(1, 2, 3) ⋅ sl.vector(2, 3, 4) == 20
-        @test sl.vector(1, 2, 3) × sl.vector(2, 3, 4) == sl.vector(-1, 2, -1)
-        @test sl.vector(2, 3, 4) × sl.vector(1, 2, 3) == sl.vector(1, -2, 1)
+        @test vector([1, 2, 3]) ⋅ vector([2, 3, 4]) == 20
+        @test vector([1, 2, 3] × [2, 3, 4]) == vector([-1, 2, -1])
+        @test vector([2, 3, 4] × [1, 2, 3]) == vector([1, -2, 1])
     end
 
     @testset "ch 2 - drawing on a canvas" begin
