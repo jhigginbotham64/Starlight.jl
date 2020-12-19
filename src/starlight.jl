c77fc5b0cadb474008ed2b47c7acd8d098ff91c0
Joshua Higginbotham <jhigginbotham64@gmail.com>
2020-12-19 14:20:57 -0600

    major refactor, ch2 finished with tests passing. chatted with HoJ discord and was reminded that julia notebooks can display color matrices. got that working and realized that i can also export those displays as files. no need to bother with PPM now, and also no need for SDL...really at all, altho i may find some way to work it in. before i call this chapter done, i'm going to go back and rewrite all the color tests to use the appropriate julia packages and make sense in that context, it would be cheating to be doing something different with my canvas than i do with my basic colors, also it would quickly get confusing.

--- src/starlight.jl
+++ src/starlight.jl
@@ -2,21 +2,16 @@ module starlight
 
 using Colors
 
-#=
-    points and vectors are just length-4 arrays with particular values
-    in the last index
-=#
-
 export fitn
-export point
-export vector
-export color
-export sdl_colors
-export canvas
+export F, VectorF, Point4, Vector4
+export BlankCanvas
+export pixel, pixel!
+export x, x!, y, y!, z, z!, height, width
+export pixels, flat
 
 function fitn(vec = [], n::Int = 3)
     """
-        fit vector to n elements, i.e. truncate or pad.
+        fit Vector4 to n elements, i.e. truncate or pad.
         defaults to n = 3 because that's my use case.
     """
     if length(vec) == 0
@@ -26,153 +21,55 @@ function fitn(vec = [], n::Int = 3)
     end
 end
 
-function point(coords = [])
+# Point4 and Vector4 are just length-4 arrays with particular valuesin the last index
+
+function Point4(coords = [])
     return vcat(fitn(coords), [AbstractFloat(1)])
 end
 
-color = point # can be a simple alias due to how we handle points
-
-function vector(coords = [])
+function Vector4(coords = [])
     return vcat(fitn(coords), [AbstractFloat(0)])
 end
 
-#=
-    really all the important matrix operations can be done directly
-    with julia's built-in types, but we also like to be able to refer
-    to coordinate axes by name. fortunately julia makes it relatively
-    simple to add that on top of its builtins. the same holds for color
-    components.
-=#
+F = T where T<:AbstractFloat
+VectorF = Vector{T} where T<:AbstractFloat
 
-function Base.getproperty(vec::Vector{<:AbstractFloat}, sym::Symbol)
-    if sym === :x || sym === :red || sym === :r
-        if length(vec) >= 1
-            return vec[1]
-        else
-            @warn "property $(String(sym)) is not defined for empty arrays"
-            return nothing
-        end
-    elseif sym === :y || sym === :green || sym === :g
-        if length(vec) >= 2
-            return vec[2]
-        else
-            @warn "property $(String(sym)) is not defined for arrays with fewer than 2 elements"
-            return nothing
-        end
-    elseif sym === :z || sym === :blue || sym === :b
-        if length(vec) >= 3
-            return vec[3]
-        else
-            @warn "property $(String(sym)) is not defined for arrays with fewer than 3 elements"
-            return nothing
-        end
-    elseif sym === :w || sym === :alpha || sym === :a
-        if length(vec) >= 4
-            return vec[4]
-        else
-            @warn "property $(String(sym)) is not defined for arrays with fewer than 4 elements"
-            return nothing
-        end
-    elseif sym === :colorant
-        # it just happens that the point function impelements sensible
-        # defaults if we want to represent a shorter array as a color
-        clamped = clamp.(vec, 0, 1)
-        cols = (length(vec) >= 4) ? clamped : point(clamped)
-        return RGBA(cols...)
+function GetIndexOrWarn(vec::VectorF, i::Int, sym::Symbol)
+    if length(vec) >= i
+        return vec[i]
     else
-        return getfield(vec, sym)
+        @warn "vector must have length at least $(String(i)) to interpret index $(String(i)) as its $(String(sym)) component"
+        return nothing
     end
 end
 
-function Base.setproperty!(vec::Vector{<:AbstractFloat}, sym::Symbol, val::T where T<:AbstractFloat)
-    if sym === :x || sym === :red || sym === :r
-        if length(vec) >= 1
-            vec[1] = val
-        else
-            @warn "property $(String(sym)) is not defined for empty arrays"
-        end
-    elseif sym === :y || sym === :green || sym === :g
-        if length(vec) >= 2
-            vec[2] = val
-        else
-            @warn "property $(String(sym)) is not defined for arrays with fewer than 2 elements"
-        end
-    elseif sym === :z || sym === :blue || sym === :b
-        if length(vec) >= 3
-            vec[3] = val
-        else
-            @warn "property $(String(sym)) is not defined for arrays with fewer than 3 elements"
-        end
-    elseif sym === :w || sym === :alpha || sym === :a
-        if length(vec) >= 4
-            vec[4] = val
-        else
-            @warn "property $(String(sym)) is not defined for arrays with fewer than 4 elements"
-        end
+x(vec::VectorF) = GetIndexOrWarn(vec, 1, :x)
+y(vec::VectorF) = GetIndexOrWarn(vec, 2, :y)
+z(vec::VectorF) = GetIndexOrWarn(vec, 3, :z)
+w(vec::VectorF) = GetIndexOrWarn(vec, 4, :w)
+
+function SetIndexOrWarn!(vec::VectorF, i::Int, sym::Symbol, val::T where T<:AbstractFloat)
+    if length(vec) >= i
+        vec[i] = val
     else
-        setfield!(vec, sym, val)
+        @warn "vector must have length at least $(String(i)) to interpret index $(String(i)) as its $(String(sym)) component"
+        return nothing
     end
 end
 
-# first bit of code i've written for this project where i was
-# like "idk if i really like this or not", but whatever
-sdl_colors(c::Vector{<:AbstractFloat}) = sdl_colors(c.colorant)
-# shamelessly copied from GameZero, hurrah for open source
-sdl_colors(c::Colorant) = sdl_colors(convert(ARGB{Colors.FixedPointNumbers.Normed{UInt8,8}}, c))
-sdl_colors(c::ARGB) = Int.(reinterpret.((red(c), green(c), blue(c), alpha(c))))
+x!(vec::VectorF, val::F) = SetIndexOrWarn!(vec, 1, :x, val)
+y!(vec::VectorF, val::F) = SetIndexOrWarn!(vec, 2, :y, val)
+z!(vec::VectorF, val::F) = SetIndexOrWarn!(vec, 3, :z, val)
+w!(vec::VectorF, val::F) = SetIndexOrWarn!(vec, 4, :w, val)
 
-#=
-    in the book, a canvas is a 3D array where the first 2 dimensions are height
-    and width (perhaps not in that order) and the 3rd dimension is color. you
-    use x,y coordinates to write colors.
-
-    an empty canvas is initialized with color([]) which comes out to opaque
-    black. the book also wants it to have width and height properties, as well
-    as getters and setters for individual pixels (pixel_at and write_pixel).
-
-    the workflow used in the book is to create a canvas, manipulate it, and save
-    the results to PPM. the workflow i want is to create a canvas, manipulate it,
-    and show it in an SDL window, and optionally save it to...something other than
-    PPM, probably using an image library.
-
-    it had not occurred to me that these first few chapters would be the hardest
-    because of things like this, and were the last time i tried as well.
-
-    the SDL stuff could be done using module-level state. since the canvas is
-    maintained by the caller, the image saving would not require state unless
-    i wanted to export from SDL using SaveBMP, which i'd kinda like to.
-
-    ok so our canvas is going to be a w * h (or h * w, idk quite yet) * 4 Array,
-    so that type needs to have width and height properties defined. idk if julia
-    allows for a nicer way to do this, but i also think it would be helpful to
-    have a "linear" property that returns the array reshaped to (w * h) * 4 so
-    you can iterate over all pixels in a single unnested loop.
-=#
-
-#=
-    again, canvases are just special matrices and we can layer the stuff
-    we need for the test cases on top of native types. no setproperty! this
-    time because all the properties are read-only properties of the underlying
-    matrix, or else views onto it that wouldn't make sense to directly assign.
-    (but see https://docs.julialang.org/en/v1/base/arrays/#Base.reshape, reshape
-    returns a result that shares the same underlying data as the original
-    matrix, so you can still "do stuff" with it).
-=#
-
-function canvas(w::Int, h::Int)
-    # height is number of rows, which in julia is the first dimension.
-    # width is number of columns, which in julia is the second dimension.
-    return fill(color(), (h, w))
-end
-
-function Base.getproperty(mat::Array{Vector{T},2} where T<:AbstractFloat, sym::Symbol)
-    if sym == :height || sym == :h
-        return size(mat)[1]
-    elseif sym == :width || sym == :w
-        return size(mat)[2]
-    elseif sym == :linear || sym == :pixels
-        return reshape(mat, (prod(size(mat)), 1))
-    end
-end
+# height is number of rows, which in julia is the first dimension.
+# width is number of columns, which in julia is the second dimension.
+BlankCanvas(w::Int, h::Int) = fill(colorant"black", (h, w))
+height(mat) = size(mat)[1]
+width(mat) = size(mat)[2]
+pixel(mat, x::Int, y::Int) = mat[x,y]
+pixel!(mat, x::Int, y::Int, c::Colorant) = mat[x,y] = c
+pixels(mat) = flat(mat)
+flat(mat) = reshape(mat, (prod(size(mat)), 1))
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,150 +3,94 @@ sl = starlight
 
 using Test
 using LinearAlgebra
+using Colors
 
 @testset "ray tracer challenge" begin
 
-    @testset "ch 1 - tuples, points, and vectors" begin
-
-        #=
-            ch 1 summary:
-
-            julia's native data types and standard library functions
-            turned this chapter into a smoke test of basic language features.
-            very different experience from python, but that may because i'm
-            older and have already done it once in python and am less inclined
-            to go off the rails.
-
-            it takes less than 100 lines of module code to make all these
-            tests pass. define getproperty and setproperty! for arrays of
-            numbers so you can use .x, .y, etc. then define point and vector
-            "constructors" which are basically just functions that take an
-            array and return the first 3 elements (or the list 0-padded to
-            length 3) and tack 1 or 0 respectively onto the end. tuples as
-            defined by the book become superfluous, even a hindrance, and this
-            is why we now have test cases which are basically just "julia can do
-            arithmetic on arrays, yaaaaaaay".
-
-            the one weird spot is that the standard library cross product is
-            only defined for arrays of length 3. the book's cross product is...
-            the same, but is only defined for vectors and ignores the last
-            element. i opted to cheat a little and just use the standard library
-            cross product without "creating a vector" first (see the last two
-            tests for what i mean). this is simple enough but requires me or
-            whoever else to remember something extra about how this code works.
-            i may come up with something else depending on how much i have to
-            use and abuse the cross product in this book.
-
-            whatever i do, my goal for now is to avoid breaking and/or
-            revisiting previous tests as much as possible, so there's a good
-            chance that it (and everything else from this chapter) will stay
-            the way it is.
-
-            i can't fault the book though. it's written to be language-agnostic,
-            and the same content has still proved very valuable in helping me
-            learn some of the julia fundamentals. remember, this is my first
-            time using this language. overall i'm fairly pleased with it so far.
-        =#
+    @testset "ch 1 - tuples, Point4s, and Vector4s" begin
 
         vals = [4.3, -4.2, 3.1, 1.0]
 
         a = copy(vals)
-        @test a.x == 4.3
-        @test a.y == -4.2
-        @test a.z == 3.1
-        @test a.w == 1.0
-        @test a == point(vals)
-        @test a != vector(vals)
+        @test x(a) == 4.3
+        @test y(a) == -4.2
+        @test z(a) == 3.1
+        @test w(a) == 1.0
+        @test a == Point4(vals)
+        @test a != Vector4(vals)
 
-        a.w = 0.0
-        @test a.x == 4.3
-        @test a.y == -4.2
-        @test a.z == 3.1
-        @test a.w == 0.0
-        @test a != point(vals)
-        @test a == vector(vals)
+        w!(a, 0.0)
+        @test x(a) == 4.3
+        @test y(a) == -4.2
+        @test z(a) == 3.1
+        @test w(a) == 0.0
+        @test a != Point4(vals)
+        @test a == Vector4(vals)
 
-        @test point([4, -4, 3]) == [4, -4, 3, 1]
-        @test vector([4, -4, 3]) == [4, -4, 3, 0]
+        @test Point4([4, -4, 3]) == [4, -4, 3, 1]
+        @test Vector4([4, -4, 3]) == [4, -4, 3, 0]
         @test [3, -2, 5, 1] + [-2, 3, 1, 0] == [1, 1, 6, 1]
-        @test point([3, 2, 1]) - point([5, 6, 7]) == vector([-2, -4, -6])
-        @test point([3, 2, 1]) - vector([5, 6, 7]) == point([-2, -4, -6])
-        @test vector([3, 2, 1]) - vector([5, 6, 7]) == vector([-2, -4, -6])
-        @test vector([0, 0, 0]) - vector([1, -2, 3]) == vector([-1, 2, -3])
+        @test Point4([3, 2, 1]) - Point4([5, 6, 7]) == Vector4([-2, -4, -6])
+        @test Point4([3, 2, 1]) - Vector4([5, 6, 7]) == Point4([-2, -4, -6])
+        @test Vector4([3, 2, 1]) - Vector4([5, 6, 7]) == Vector4([-2, -4, -6])
+        @test Vector4([0, 0, 0]) - Vector4([1, -2, 3]) == Vector4([-1, 2, -3])
         @test -[1, -2, 3, -4] == [-1, 2, -3, 4]
         @test 3.5 * [1, -2, 3, -4] == [3.5, -7, 10.5, -14]
         @test 0.5 * [1, -2, 3, -4] == [0.5, -1, 1.5, -2]
         @test [1, -2, 3, -4] / 2 == [0.5, -1, 1.5, -2]
 
-        @test norm(vector([1, 0, 0])) == 1
-        @test norm(vector([0, 1, 0])) == 1
-        @test norm(vector([0, 0, 1])) == 1
-        @test norm(vector([1, 2, 3])) == √14 # i love julia
-        @test norm(vector([-1, -2, -3])) == √14
+        @test norm(Vector4([1, 0, 0])) == 1
+        @test norm(Vector4([0, 1, 0])) == 1
+        @test norm(Vector4([0, 0, 1])) == 1
+        @test norm(Vector4([1, 2, 3])) == √14 # i love julia
+        @test norm(Vector4([-1, -2, -3])) == √14
 
-        @test normalize(vector([4, 0, 0])) == vector([1, 0, 0])
-        @test normalize(vector([1, 2, 3])) == vector([1/√14, 2/√14, 3/√14]) # i really love julia
-        @test norm(normalize(vector([1, 2, 3]))) == 1
+        @test normalize(Vector4([4, 0, 0])) == Vector4([1, 0, 0])
+        @test normalize(Vector4([1, 2, 3])) == Vector4([1/√14, 2/√14, 3/√14]) # i really love julia
+        @test norm(normalize(Vector4([1, 2, 3]))) == 1
 
         # i really really really love julia
-        @test vector([1, 2, 3]) ⋅ vector([2, 3, 4]) == 20
-        @test vector([1, 2, 3] × [2, 3, 4]) == vector([-1, 2, -1])
-        @test vector([2, 3, 4] × [1, 2, 3]) == vector([1, -2, 1])
+        @test Vector4([1, 2, 3]) ⋅ Vector4([2, 3, 4]) == 20
+        @test Vector4([1, 2, 3] × [2, 3, 4]) == Vector4([-1, 2, -1])
+        @test Vector4([2, 3, 4] × [1, 2, 3]) == Vector4([1, -2, 1])
     end
 
     @testset "ch 2 - drawing on a canvas" begin
 
-        #=
-            ch 2 summary:
-
-            again julia trivializes things that were difficult in python.
-            this time i dispensed even with the "constructors" for colors,
-            because since they're usually 3-tuples instead of 4-tuples it
-            doesn't even make sense to have a shortcut. so i just added more
-            stuff to getproperty and setproperty! and voila, tests pass.
-            except for a couple which i had to adjust to use approx, because
-            computers, but even that's cleaner than what tended to happen in
-            python. can't wait to see what happens when i get to matrix ops.
-            the tests are slightly different from the ones in the book, if i
-            was to do the ones from the book it would be...more smoke-testing...
-
-
-        =#
         c = [-0.5, 0.4, 1.7]
-        @test c.red == c.r == -0.5
-        @test c.green == c.g == 0.4
-        @test c.blue == c.b == 1.7
+        @test red(c) == -0.5
+        @test green(c) == 0.4
+        @test blue(c) == 1.7
 
         c = [0.9, 0.6, 0.75] + [0.7, 0.1, 0.25]
-        @test c.r == 1.6
-        @test c.g == 0.7
-        @test c.b == 1.0
+        @test red(c) == 1.6
+        @test green(c) == 0.7
+        @test blue(c) == 1.0
 
         c = [0.9, 0.6, 0.75] - [0.7, 0.1, 0.25]
-        @test c.r ≈ 0.2 # never really know where numerical instability will strike
-        @test c.g == 0.5
-        @test c.b == 0.5
+        @test red(c) ≈ 0.2 # never really know where numerical instability will strike
+        @test green(c) == 0.5
+        @test blue(c) == 0.5
 
         c = 2 * [0.2, 0.3, 0.4]
-        @test c.r == 0.4
-        @test c.g == 0.6
-        @test c.b == 0.8
+        @test red(c) == 0.4
+        @test green(c) == 0.6
+        @test blue(c) == 0.8
 
         # the formula the book gives for color product is the hadamard product,
-        # which is component-wise multiplication. this is so trivial in julia it
-        # doesn't even warrant a separate function.
+        # which is component-wise multiplication, pretty simple in julia.
         c = [1, 0.2, 0.4] .* [0.9, 1, 0.1]
-        @test c.r == 0.9
-        @test c.g == 0.2
-        @test c.b ≈ 0.04 # again with the instability, and again it's easier to handle in julia than python
+        @test red(c) == 0.9
+        @test green(c) == 0.2
+        @test blue(c) ≈ 0.04 # again with the instability, and again it's easier to handle in julia than python
 
-        c = canvas(10, 20)
-        @test c.w == c.width == 10
-        @test c.h == c.height == 20
-        @test all(col == color([0,0,0]) for col in c.linear)
+        c = BlankCanvas(10, 20)
+        @test width(c) == 10
+        @test height(c) == 20
+        @test all(col == colorant"black" for col in pixels(c))
 
-        # write_pixel(c, 2, 3, color([1, 0, 0]))
-        # @test pixel_at(c, 2, 3) == color([1, 0, 0])
+        pixel!(c, 2, 3, colorant"red")
+        @test pixel(c, 2, 3) == colorant"red"
     end
 
     @testset "ch 3 - matrices" begin
