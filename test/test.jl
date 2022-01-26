5b522309a133fabcd2fc98150e97db0b88927fef
Joshua Higginbotham <jhigginbotham64@pop-os.localdomain>
2022-01-25 18:00:03 -0600

    small edit

--- test/test.jl
+++ test/test.jl
@@ -4,9 +4,6 @@ using Starlight
 # load test file
 a = App("test/test.yml")
 
-# make sure we didn't silently fail be verifying config values
-print(a)
-
 # kick off
-# run with JULIA_DEBUG=all to see clock messages
+# run with JULIA_DEBUG=Starlight to see clock messages
 awake(a)
