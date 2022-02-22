91602839d1d0e329a54e69c44bea9231cc8ca3db
Joshua Higginbotham <jhigginbotham64@pop-os.localdomain>
2022-02-22 17:58:07 -0600

    getting ready for the big switch to C++

--- src/Entities.jl
+++ src/Entities.jl
@@ -15,15 +15,9 @@ mutable struct ColorLine <: Renderable
   end
 end
 
-# CPP draw line (color, p1x, p1y, p2x, p2y)
+# CPP draw line
 function draw(l::ColorLine)
-  SDL_SetRenderDrawColor(
-      sdl.rnd,
-      sdl_colors(l.color)...,
-  )
-  SDL_RenderDrawLine(sdl.rnd, Cint.((
-    l.p1[1]+l.abs_pos.x, l.p1[2]+l.abs_pos.y, 
-    l.p2[1]+l.abs_pos.x, l.p2[2]+l.abs_pos.y))...)
+  
 end
 
 mutable struct ColorRect <: Renderable
@@ -32,23 +26,9 @@ mutable struct ColorRect <: Renderable
   end
 end
 
-Base.convert(T::Type{SDL_Rect}, r::ColorRect) = SDL_Rect(
-  Cint.((r.p[1]+r.abs_pos.x-Int(round(r.w/2)), 
-        r.p[2]+r.abs_pos.y-Int(round(r.h/2)), 
-        r.w, r.h))...)
-
-# CPP draw rectangle (color, cx, cy, w, h)
+# CPP draw rectangle
 function draw(r::ColorRect)
-  SDL_SetRenderDrawColor(
-      sdl.rnd,
-      sdl_colors(r.color)...,
-  )
-  sr = convert(SDL_Rect, r)
-  if !r.fill
-      SDL_RenderDrawRect(sdl.rnd, Ref(sr))
-  else
-      SDL_RenderFillRect(sdl.rnd, Ref(sr))
-  end
+  
 end
 
 mutable struct ColorCirc <: Renderable
@@ -57,53 +37,8 @@ mutable struct ColorCirc <: Renderable
   end
 end
 
-# CPP draw circle (color, cx, cy, r)
+# CPP draw circle
 function draw(circle::ColorCirc)
-  # define the center and needed sides of circle
-  centerX = Cint(circle.p[1]+circle.abs_pos.x)
-  centerY = Cint(circle.p[2]+circle.abs_pos.y)
-  int_rad = Cint(circle.r)
-  left = centerX - int_rad
-  top = centerY - int_rad
-
-  SDL_SetRenderDrawColor(
-      sdl.rnd,
-      sdl_colors(circle.color)...,
-  )
-
-  # we consider a grid with sides equal to the circle's diameter
-  for x in left:centerX
-    for y in top:centerY
-
-      # for each pixel in the top left quadrant of the grid we measure the distance from the center.
-      dist = sqrt( (centerX - x)^2 + (centerY - y)^2 )
-
-      # if it is close to the circle's radius it and all associated points in the other quadrants are colored in.
-      if (dist <= circle.r + 0.5 && dist >= circle.r - 0.5)
-        rel_x = centerX - x
-        rel_y = centerY - y
-
-        quad1 = (x              , y              )
-        quad2 = (centerX + rel_x, y              )
-        quad3 = (x              , centerY + rel_y)
-        quad4 = (quad2[1]       , quad3[2]       )
-
-        SDL_RenderDrawPoint(sdl.rnd, quad1[1], quad1[2])
-        SDL_RenderDrawPoint(sdl.rnd, quad2[1], quad2[2])
-        SDL_RenderDrawPoint(sdl.rnd, quad3[1], quad3[2])
-        SDL_RenderDrawPoint(sdl.rnd, quad4[1], quad4[2])
-
-        # if we are told to fill in the circle we draw lines between all of the quadrants to completely fill the circle
-        if (circle.fill)
-          SDL_RenderDrawLine(sdl.rnd, quad1[1], quad1[2], quad2[1], quad2[2])
-          SDL_RenderDrawLine(sdl.rnd, quad2[1], quad2[2], quad4[1], quad4[2])
-          SDL_RenderDrawLine(sdl.rnd, quad4[1], quad4[2], quad3[1], quad3[2])
-          SDL_RenderDrawLine(sdl.rnd, quad3[1], quad3[2], quad1[1], quad1[2])
-        end
-      end
-
-    end
-  end
 
 end
 
@@ -113,61 +48,9 @@ mutable struct ColorTri <: Renderable
   end
 end
 
-# CPP draw triangle (color, p1x, p1y, p2x, p2y, p3x, p3y)
+# CPP draw triangle
 function draw(tr::ColorTri)
-  p1 = Cint.([tr.p1[1]+tr.abs_pos.x, tr.p1[2]+tr.abs_pos.y])
-  p2 = Cint.([tr.p2[1]+tr.abs_pos.x, tr.p2[2]+tr.abs_pos.y])
-  p3 = Cint.([tr.p3[1]+tr.abs_pos.x, tr.p3[2]+tr.abs_pos.y])
-  SDL_SetRenderDrawColor(sdl.rnd, sdl_colors(tr.color)...)
-  SDL_RenderDrawLines(sdl.rnd, [p1; p2; p3; p1], Cint(4))
-
-  ymax = max(p1[2], p2[2], p3[2])
-  ymin = min(p1[2], p2[2], p3[2])
-  if tr.fill && ymin != ymax
-    # Set q1, q2 and q3 in descending order of y-value
-    q1 = (p1[2] != ymax != p2[2]) * p3 +
-        (p2[2] != ymax != p3[2]) * p1 +
-        (p3[2] != ymax != p1[2]) * p2 +
-        (p1[2] == p2[2] == ymax) * p2 +
-        (p2[2] == p3[2] == ymax) * p3 +
-        (p3[2] == p1[2] == ymax) * p1
-    q3 = (p1[2] != ymin != p2[2]) * p3 +
-        (p2[2] != ymin != p3[2]) * p1 +
-        (p3[2] != ymin != p1[2]) * p2 +
-        (p1[2] == p2[2] == ymin) * p2 +
-        (p2[2] == p3[2] == ymin) * p3 +
-        (p3[2] == p1[2] == ymin) * p1
-    q2 = ((q1 == p1 && q3 == p3) || (q1 == p3 && q3 == p1)) * p2 +
-        ((q1 == p1 && q3 == p2) || (q1 == p2 && q3 == p1)) * p3 +
-        ((q1 == p2 && q3 == p3) || (q1 == p3 && q3 == p2)) * p1
-
-    n = q1[2] - q2[2]
-    x0 = q1[1] + (q2[2] - q1[2]) / (q3[2] - q1[2]) * (q3[1] - q1[1])
-    for j = Cint(0):n-Cint(1)
-      r1 = [round(Cint, q2[1] + j / n * (q1[1] - q2[1])); q2[2] + j]
-      r2 = [round(Cint, x0 + j / n * (q1[1] - x0)); q2[2] + j]
-      SDL_RenderDrawLines(sdl.rnd, [r1; r2], Cint(2))
-    end
-    n = q2[2] - q3[2]
-    for j = Cint(1):n-Cint(1)
-      r1 = [round(Cint, q2[1] + j / n * (q3[1] - q2[1])); q2[2] - j]
-      r2 = [round(Cint, x0 + j / n * (q3[1] - x0)); q2[2] - j]
-      SDL_RenderDrawLines(sdl.rnd, [r1; r2], Cint(2))
-    end
-  end
-end
-
-function Base.size(s::Ptr{SDL_Surface})
-  ss = unsafe_load(s)
-  return (ss.w, ss.h)
-end
-
-function image_surface(img)
-  sf = IMG_Load(img)
-  if sf == C_NULL
-    throw("Error loading $image_file")
-  end
-  return sf
+  
 end
 
 mutable struct Sprite <: Renderable
@@ -188,53 +71,9 @@ mutable struct Sprite <: Renderable
   end
 end
 
-# CPP draw sprite (img, alpha, src, dst, rot z)
+# CPP draw sprite
 function draw(s::Sprite)
-  srf = image_surface(s.img)
-  w, h = size(srf)
-  txt = SDL_CreateTextureFromSurface(sdl.rnd, srf)
-
-  if (s.alpha < 255)
-    SDL_SetTextureBlendMode(txt, SDL_BLENDMODE_BLEND)
-    SDL_SetTextureAlphaMod(txt, s.alpha)
-  end
-
-  # calculations for source
-  if s.region != [0, 0, 0, 0]
-    srctl = [s.region[1], s.region[2]]
-    srcw, srch = [s.region[3], s.region[4]]
-  elseif s.cell_size != [0, 0]
-    srctl = [s.cell_ind[2] * s.cell_size[1], s.cell_ind[1] * s.cell_size[2]]
-    srcw, srch = s.cell_size
-  else
-    srctl = [0, 0]
-    srcw, srch = [w, h]
-  end
-
-  # calculations for destination
-  if s.region != [0, 0, 0, 0]
-    dstw, dsth = s.region[3], s.region[4]
-  elseif s.cell_size != [0, 0]
-    dstw, dsth = s.cell_size
-  else
-    dstw, dsth = [w, h]
-  end
-
-  dstw = floor(dstw * s.scale.x)
-  dsth = floor(dsth * s.scale.y)
   
-  SDL_RenderCopyEx(
-    sdl.rnd, 
-    txt, 
-    Ref(SDL_Rect(srctl[1], srctl[2], srcw, srch)),
-    Ref(SDL_Rect(s.abs_pos.x-Int(round(dstw/2)), s.abs_pos.y-Int(round(dsth/2)), dstw, dsth)),
-    s.abs_rot.z, # NOTE angles are in degrees, positive is clockwise
-    C_NULL,
-    SDL_FLIP_NONE
-  )
-
-  SDL_DestroyTexture(txt)
-  SDL_FreeSurface(srf)
 end
 
 mutable struct Text <: Renderable
@@ -246,32 +85,7 @@ mutable struct Text <: Renderable
   end
 end
 
-# CPP draw text (fname, fsize, color, alpha, dst, rot z)
+# CPP draw text
 function draw(t::Text)
-  font = TTF_OpenFont(t.font_name, t.font_size)
-  srf = TTF_RenderText_Blended(font, t.text, SDL_Color(sdl_colors(t.color)...))
-  w, h = size(srf)
-
-  w = floor(w * t.scale.x)
-  h = floor(h * t.scale.y)
-  txt = SDL_CreateTextureFromSurface(sdl.rnd, srf)
-
-  if t.alpha < 255
-    SDL_SetTextureBlendMode(txt, SDL_BLENDMODE_BLEND)
-    SDL_SetTextureAlphaMod(txt, t.alpha)
-  end
   
-  SDL_RenderCopyEx(
-    sdl.rnd, 
-    txt, 
-    C_NULL,
-    Ref(SDL_Rect(t.abs_pos.x-Int(round(w/2)), t.abs_pos.y-Int(round(h/2)), w, h)),
-    t.abs_rot.z,
-    C_NULL,
-    SDL_FLIP_NONE
-  )
-
-  SDL_DestroyTexture(txt)
-  SDL_FreeSurface(srf)
-  TTF_CloseFont(font)
 end
--- src/SDL.jl
+++ src/SDL.jl
@@ -22,19 +22,12 @@ function draw()
   clear()
   map(draw, scn) # TODO investigate parallelization
   # CPP present
-  SDL_RenderPresent(sdl.rnd)
 end
 
 function handleMessage(s::SDL, m::TICK)
   @debug "SDL tick"
   try
-    # handle events
-    # CPP poll events
-    event_ref = Ref{SDL_Event}()
-    while Bool(SDL_PollEvent(event_ref))
-      evt = event_ref[]
-      sendMessage(evt)
-    end
+    # TODO CPP events
 
     # draw the scene
     draw()
@@ -42,12 +35,6 @@ function handleMessage(s::SDL, m::TICK)
     handleException()
   end
 
-end
-
-# CPP remove
-function getSDLError()
-  x = SDL_GetError()
-  return unsafe_string(x)
 end
 
 to_ARGB(c) = c
@@ -61,43 +48,11 @@ sdl_colors(c::ARGB) = Int.(reinterpret.((red(c), green(c), blue(c), alpha(c))))
 
 # CPP fill (color)
 function Base.fill(c::Colorant)
-  SDL_SetRenderDrawColor(
-      sdl.rnd,
-      sdl_colors(c)...,
-  )
-  SDL_RenderClear(sdl.rnd)
+  
 end
 
 # CPP init
 function awake!(s::SDL)
-  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 4)
-  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4)
-  r = SDL_Init(SDL_INIT_EVERYTHING)
-  if r != 0
-    error("unable to initialize SDL: $(getSDLError())")
-  end
-
-  TTF_Init()
-
-  mix_init_flags = MIX_INIT_FLAC|MIX_INIT_MP3|MIX_INIT_OGG
-  inited = Mix_Init(Int32(mix_init_flags))
-  if inited & mix_init_flags != mix_init_flags
-      @warn "Failed to initialise audio mixer properly. All sounds may not play correctly\n$(getSDLError())"
-  end
-
-  device = Mix_OpenAudio(Int32(22050), UInt16(MIX_DEFAULT_FORMAT), Int32(2), Int32(1024) )
-  if device != 0
-      @warn "No audio device available, sounds and music will not play.\n$(getSDLError())"
-      Mix_CloseAudio()
-  end
-
-  # other fields set inside App constructor
-  sdl.win = SDL_CreateWindow(sdl.ttl,
-    Int32(SDL_WINDOWPOS_CENTERED), Int32(SDL_WINDOWPOS_CENTERED), Int32(sdl.wdth), Int32(sdl.hght),
-    UInt32(SDL_WINDOW_ALLOW_HIGHDPI|SDL_WINDOW_OPENGL|SDL_WINDOW_SHOWN));
-  SDL_SetWindowMinimumSize(sdl.win, Int32(sdl.wdth), Int32(sdl.hght))
-  sdl.rnd = SDL_CreateRenderer(sdl.win, Int32(-1), UInt32(SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC))
-  SDL_SetRenderDrawBlendMode(sdl.rnd, SDL_BLENDMODE_BLEND)
 
   draw()
 
@@ -106,17 +61,6 @@ end
 
 # CPP quit
 function shutdown!(s::SDL)  
-  SDL_DelEventWatch(window_event_watcher_cfunc[], sdl.win);
-  SDL_DestroyRenderer(sdl.rnd)
-  SDL_DestroyWindow(sdl.win)
-
-  Mix_HaltMusic()
-  Mix_HaltChannel(Int32(-1))
-  Mix_CloseAudio()
-
-  TTF_Quit()
-  Mix_Quit()
-  SDL_Quit()
 
   return false
 end
